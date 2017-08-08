7424d2581e4ec9d331bafe4633c21c9f155ab6a6
lscspirit <lscspirit@hotmail.com>
2017-08-09 00:51:13 +0800

    Redirect root to /payments

--- server/router.js
+++ server/router.js
@@ -5,6 +5,10 @@ import PaymentsController from "~/server/controllers/payments_controller";
 
 const router = express.Router();
 
+router.get("/", function(req, res, next) {
+  // redirect root to "/payments"
+  res.redirect("/payments");
+});
 router.use("/payments", PaymentsController);
 
 export default router;
